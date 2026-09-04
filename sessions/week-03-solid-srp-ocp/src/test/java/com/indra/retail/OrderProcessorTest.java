bf7d65a82223664ca6c08b8165e6e9f30db3335d
Reyes Montes <afreyes@indracompany.com>
2026-09-04 17:18:11 -0500

    Tarea 2: Se implementa lógica para descuento LOYALTY

---
+++ sessions/week-03-solid-srp-ocp/src/test/java/com/indra/retail/OrderProcessorTest.java
@@ -0,0 +1,33 @@
+package com.indra.retail;
+
+//import com.indra.retail.DiscountCalculator;
+import org.junit.jupiter.api.Test;
+
+import com.indra.retail.DiscountCalculator;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.math.BigDecimal;
+public class OrderProcessorTest {
+
+    private OrderProcessor orderProcessor;
+    private StockValidator stockValidator;
+    private OrderNotifier orderNotifier;
+
+    @BeforeEach
+    public void beforeTests() {
+        stockValidator = new StockValidator();
+        orderNotifier = new OrderNotifier();
+        orderProcessor = new OrderProcessor(stockValidator, orderNotifier);
+    }
+
+    @Test
+    public void testLoyaltyDiscount() {
+        assertNotNull(this.orderProcessor);
+        Order order = new Order("TR435", new BigDecimal(100L), DiscountType.LOYALTY, 2, "andrusoft@mail.com");
+        BigDecimal expectedValue = new BigDecimal(85L);
+        BigDecimal total = orderProcessor.process(order, 4);
+        assertEquals(expectedValue.longValue(), total.longValue());
+    }
+}
